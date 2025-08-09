4a5920990b47f491ef858c7b08f94c9ada9f4451
Carlos Ayala <79550716+3dukarlos@users.noreply.github.com>
2025-08-09 08:38:47 -0500

    Delete api directory

--- api/health.py
+++
@@ -1,9 +0,0 @@
-import json
-def handler(request, response):
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    response.headers['Access-Control-Allow-Methods'] = 'GET, OPTIONS'
-    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
-    if request.method == 'OPTIONS':
-        response.status_code = 204; response.body=''; return
-    response.status_code = 200
-    response.body = json.dumps({"ok": True, "runtime": "python-handler"})
