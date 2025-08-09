222ca9a2caf1ca65ef49d917e62d4be8fceb2c24
Carlos Ayala <79550716+3dukarlos@users.noreply.github.com>
2025-08-09 11:21:51 -0500

    Delete api/health.py

--- api/health.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, jsonify
-
-app = Flask(__name__)
-
-@app.route("/", methods=["GET"])
-def health():
-    return jsonify(ok=True, runtime="python", framework="flask", file="health.py")
